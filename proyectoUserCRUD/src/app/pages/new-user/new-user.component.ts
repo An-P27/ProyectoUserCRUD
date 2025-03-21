d59b70552bd168ca64766a0398e2e38a10ab6cb0
Andrea <andrea.palanco@gmail.com>
2025-03-21 14:42:59 +0100

    Formulario y validador requerido
    He empezado a pintar el formulario de nuevo usuario con los validadores de campo requerido.

--- proyectoUserCRUD/src/app/pages/new-user/new-user.component.ts
+++ proyectoUserCRUD/src/app/pages/new-user/new-user.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-user.component.html',
-  styleUrl: './new-user.component.css'
+  styleUrl: './new-user.component.css',
 })
 export class NewUserComponent {
+  userForm: FormGroup;
 
+  constructor() {
+    this.userForm = new FormGroup(
+      {
+        first_name: new FormControl('', [Validators.required]),
+        last_name: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required]),
+        image: new FormControl('', [Validators.required]),
+      },
+      []
+    );
+  }
+
+  getDataForm() {}
 }
